708bd8a1c06d8955f472e05d09a164caee9c0be9
Gul-e-Rana <144825511+Gul-e-Rana1@users.noreply.github.com>
2024-09-04 12:08:50 +0000

    Changes made

--- streamlitApp.py
+++ streamlitApp.py
@@ -29,7 +29,7 @@ st.write(f"Model {accuracy}")
 st.header("Real-Time Prediction")
 input_data = []
 for col in X_test.columns :
-    input_value = st.number_input(f'Input for feature {col}', value='0.0') 
+    input_value = st.number_input(f'Input for feature {col}', value=0) 
     input_data.append(input_value)
 # Convert input data to Dataframe
 input_df = pd.DataFrame([input_data], columns=X_test.columns)
